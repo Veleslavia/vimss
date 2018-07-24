59d6a6659a2d66266b9c41220e909ed1a9808b82
veleslavia <okalistratova@gmail.com>
2018-07-24 14:17:13 +0900

    Update master

--- Evaluate.py
+++ Evaluate.py
@@ -214,35 +214,3 @@ def draw_violin_sdr(json_folder):
 
     fig.set_size_inches(8, 3.)
     fig.savefig("sdr_histogram.pdf", bbox_inches='tight')
-
-def draw_spectrogram(example_wav="musb_005_angela thomas wade_audio_model_without_context_cut_28234samples_61002samples_93770samples_126538.wav"):
-    y, sr = librosa.load(example_wav, sr=None)
-    spec = np.abs(librosa.stft(y, 512, 256, 512))
-    norm_spec = librosa.power_to_db(spec**2)
-    black_time_frames = np.array([28234, 61002, 93770, 126538]) / 256.0
-
-    fig, ax = plt.subplots()
-    img = ax.imshow(norm_spec)
-    plt.vlines(black_time_frames, [0, 0, 0, 0], [10, 10, 10, 10], colors="red", lw=2, alpha=0.5)
-    plt.vlines(black_time_frames, [256, 256, 256, 256], [246, 246, 246, 246], colors="red", lw=2, alpha=0.5)
-
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.1)
-    plt.colorbar(img, cax=cax)
-
-    ax.xaxis.set_label_position("bottom")
-    #ticks_x = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x * 256.0 / sr))
-    #ax.xaxis.set_major_formatter(ticks_x)
-    ax.xaxis.set_major_locator(ticker.FixedLocator(([i * sr / 256. for i in range(len(y)//sr + 1)])))
-    ax.xaxis.set_major_formatter(ticker.FixedFormatter(([str(i) for i in range(len(y)//sr + 1)])))
-
-    ax.yaxis.set_major_locator(ticker.FixedLocator(([float(i) * 2000.0 / (sr/2.0) * 256. for i in range(6)])))
-    ax.yaxis.set_major_formatter(ticker.FixedFormatter([str(i*2) for i in range(6)]))
-
-    ax.set_xlabel("t (s)")
-    ax.set_ylabel('f (KHz)')
-
-    fig.set_size_inches(7., 3.)
-    fig.savefig("spectrogram_example.pdf", bbox_inches='tight')
-
-#compute_mean_metrics("/mnt/windaten/Source_Estimates/endtoend/", False)
